f23489b4346bc0234379e569c9c5f75fde03bca1
EmanuelaPau <uapame@gmail.com>
2023-06-01 17:22:32 +0200

    Add input with v-model

--- script/my_script.js
+++ script/my_script.js
@@ -4,9 +4,11 @@ const { createApp } = Vue;
 
 createApp({
     data() {
+
         return {
-            message: 'Hello Vue!'
+            message: 'Hello Vue!',
+            imgUrl: 'https://images.unsplash.com/photo-1682685797660-3d847763208e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80'
         }
-    }
+    },
     // daassas
 }).mount('#app') 
